59c1d49209de1feb845856e1810888668e7dd27d
Tomas <tj4305@gmail.com>
2022-06-09 17:32:30 +0300

    DOM task

---
+++ javascript/DOM/app.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById("age-table"));
+
+console.log(document.getElementById("age"));
+
+console.log(document.getElementsByTagName("label"));
+
+const inputs = document.querySelectorAll("form > input");
+
+console.log(inputs[0]);
+
+console.log(inputs[4]);

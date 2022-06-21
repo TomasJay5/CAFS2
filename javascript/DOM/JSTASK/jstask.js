2e7d6b101b9e6338553de5cb56585a8b4286a8e8
Tomas <tj4305@gmail.com>
2022-06-21 18:32:47 +0300

    Modified jstask

--- javascript/DOM/JSTASK/jstask.js
+++ javascript/DOM/JSTASK/jstask.js
@@ -1,4 +1,5 @@
 // https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+//Navbar
 function navbarfunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {

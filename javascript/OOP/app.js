7789bd104badd793aa955e43ee57f89c30f37b83
Tomas <tj4305@gmail.com>
2022-06-23 18:56:29 +0300

    Added Es6task

--- javascript/OOP/app.js
+++ javascript/OOP/app.js
@@ -1,9 +1,9 @@
 // // Susikurkite objektų konstruktorių naudojant new pavadinimu Book, kuris galės kurti objektus, kurie turės šias savybes (properties): name, author, year ir metodus (naudojant prototype), kurių vienas parašys pavadinima ir autorių, o kitas parodys knygos amžių (senumą).
 
-// let books = new Book("Harry Potter and the Philosophers Stone", "J. K. Rowling", 1997);
+// // let books = new Book("Harry Potter and the Philosophers Stone", "J. K. Rowling", 1997);
 
-// console.log(books.showTitleAndAuthor());
-// console.log(books.showBookAge());
+// // console.log(books.showTitleAndAuthor());
+// // console.log(books.showBookAge());
 
 // Naudojant ES6 klases sukurkite objekto Filmas (Movie) kūrimo konstruktorių, kuris turės šias savybes: 
 

c24c9d94248f86b164b8122e17008ca1bdc112f5
Tomas <tj4305@gmail.com>
2022-06-23 18:44:52 +0300

    Added Es6task

--- javascript/OOP/Es6task.js
+++ javascript/OOP/Es6task.js
@@ -17,9 +17,11 @@ class Movie {
       this.director = director;
       this.budget = budget;
       this.income = income;
+
       this.getIntroduction = function() {
         return `Movie: ${this.name}, Year: ${this.year}, Director: ${this.director}`};
-      this.getProfit = function() {;
+      
+        this.getProfit = function() {;
         return `Profit: ${this.income - this.budget} USD.`};
     }
   }

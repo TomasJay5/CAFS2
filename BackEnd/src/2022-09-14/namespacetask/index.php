647d29794b1c6515af346cb99f3676765f84a6dd
Tomas <tj4305@gmail.com>
2022-09-15 18:20:34 +0300

    Added docs-helpers

--- BackEnd/src/2022-09-14/namespacetask/index.php
+++ BackEnd/src/2022-09-14/namespacetask/index.php
@@ -16,16 +16,22 @@ require_once __DIR__ . '/Services/Messengers/FacebookMessengerService.php';
 require_once __DIR__ . '/Services/Messengers/SmsMessengerService.php';
 
 $text = 'Hello World';
+
 $host = 'smtp.gmail.com';
 $port = 587;
 $username = 'testtest@gmail.com';
 $password = 'testtest';
+
 $emailMessaenger = new Services\Messengers\EmailMessengerService($host, $username, $password);
 $emailMessaenger->send('hello@nonamez.name', $text);
+
 $smsMessager = new Services\Messengers\SmsMessengerService();
 $smsMessager->send('+37061234567', $text);
+
 $facebookAppName = 'test-name';
 $facebookAppKey  = '';
+
 $facebookConnector = new Connectors\FacebookConnector($facebookAppName, $facebookAppKey);
+
 $facebookMessenger = new Services\Messengers\FacebookMessengerService($facebookConnector);
 $facebookMessenger->send(4, $text);
